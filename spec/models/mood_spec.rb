33b9283095be8f6b69a037cd0bba46b63c0439df
Yves Brissaud <yves.brissaud@gmail.com>
2016-04-05 13:35:27 +0200

    Describe data model
    Mood {felt_on, feeling, team}
    Note {occurred_on, body, team}
    Organization {name, users, teams, moods, notes}
    Team {name, users}

---
+++ spec/models/mood_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Mood, type: :model do
+  it { should validate_presence_of :felt_on }
+  it { should validate_presence_of :feeling }
+  it { should have_one :team }
+end
