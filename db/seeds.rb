6a5f671a8ae8485e31e3333608dca7148d3c4bba
Yves Brissaud <yves.brissaud@gmail.com>
2016-04-12 21:51:24 +0200

    Add example datas to test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+organization = Organization.create name: 'Eunomie'
+teams = Team.create [{ name: 'Koni', organization: organization },
+                     { name: 'Nout', organization: organization }]
+users = User.create [{ name: 'Global user', email: 'global@eunomie.info', password: 'global', organization: organization },
+                     { name: 'Koni user',   email: 'koni@eunomie.info',   password: 'koni',   organization: organization, team: teams[0] },
+                     { name: 'Nout user',   email: 'nout@eunomie.info',   password: 'nout',   organization: organization, team: teams[1] }]
