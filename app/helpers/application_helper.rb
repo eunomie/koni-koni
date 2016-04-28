a63e0265fd1b734e1213641901397b73edbe9964
Yves Brissaud <yves.brissaud@gmail.com>
2016-04-28 23:10:36 +0200

    Add presenter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Misc helpers
 module ApplicationHelper
+  def present(model, presenter_class = nil)
+    klass = presenter_class ||  "#{model.class}Presenter".constantize
+    presenter = klass.new model, self
+    yield(presenter) if block_given?
+  end
 end
