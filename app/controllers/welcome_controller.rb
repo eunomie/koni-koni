516aa4028905465590a5ac579c25cedef361d5bb
Yves Brissaud <yves.brissaud@gmail.com>
2016-05-12 20:12:32 +0200

    Display all stats in welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 # Welcome to KoniKoni
 class WelcomeController < ApplicationController
   def index
+    dates = Mood.dates
+    @moods = ({
+      dates[0] => [],
+      dates[1] => []
+    }).merge(Mood.by_date)
+    @presented_moods = MoodsPresenter.from @moods, view_context
   end
 end
--- app/models/mood.rb
+++ app/models/mood.rb
@@ -12,14 +12,24 @@ class Mood < ActiveRecord::Base
     bad: 'bad'
   }
 
-  def self.by_date(for_organization:, and_team: nil)
-    params = { organization: for_organization }
-    params = params.merge({ team_id: and_team.id }) unless and_team.nil?
-    Mood.where(params).group_by(&:felt_on)
+  def self.by_date(for_organization: nil, and_team: nil)
+    if for_organization.nil?
+      moods = Mood.all
+    else
+      params = { organization: for_organization }
+      params = params.merge({ team_id: and_team.id }) unless and_team.nil?
+      moods = Mood.where(params)
+    end
+    moods.group_by(&:felt_on)
   end
 
   def self.dates_for(organization)
     [Mood.where({ organization: organization }).minimum(:felt_on),
      Mood.where({ organization: organization }).maximum(:felt_on)]
+  end
+
+  def self.dates
+    [Mood.minimum(:felt_on),
+     Mood.maximum(:felt_on)]
   end
 end
