6d0554621230b7fbeec96b14fd6b40a5430119da
Yves Brissaud <yves.brissaud@gmail.com>
2016-05-18 22:16:25 +0200

    Add new home made graph

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,10 @@
 # Welcome to KoniKoni
 class WelcomeController < ApplicationController
   def index
-    dates = Mood.dates
+    @dates = Mood.dates
     @moods = ({
-      dates[0] => [],
-      dates[1] => []
+      @dates[0] => [],
+      @dates[1] => []
     }).merge(Mood.by_date)
     @presented_moods = MoodsPresenter.from @moods, view_context
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,18 @@ module ApplicationHelper
     presenter = klass.new model, self
     yield(presenter) if block_given?
   end
+
+  def class_for_mood_value(value)
+    css = case value
+          when 5..10
+            "good"
+          when -5..5
+            "normal"
+          when -10..-5
+            "bad"
+          else
+            "empty"
+          end
+    "#{css} mood-#{value.to_i}"
+  end
 end
